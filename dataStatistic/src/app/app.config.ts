9abb8a85e440da18c656e766c2cc7f5cbbf25350
Aliaksandr Ptsitsa <ptenix@gmail.com>
2025-02-05 21:42:23 +0300

    refactoring: remove useless index.ts

--- dataStatistic/src/app/app.config.ts
+++ dataStatistic/src/app/app.config.ts
@@ -5,11 +5,12 @@ import {provideStore} from '@ngrx/store';
 import {dataReducer} from './store/load-data/load-data.reducer';
 import {provideHttpClient} from '@angular/common/http';
 import {provideStoreDevtools} from '@ngrx/store-devtools';
+import {metaReducers} from './store/meta-reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideStore({data: dataReducer}),
+    provideStore({data: dataReducer}, {metaReducers}),
     provideHttpClient(),
     provideStoreDevtools({
       maxAge: 25,
